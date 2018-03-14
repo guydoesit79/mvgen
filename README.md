94d200568f39e3ac1baed349570c429682aa8a0b
None <none@none.com>
2018-03-14 20:51:47 +0000

    init

---
+++ README.md
@@ -0,0 +1,35 @@
+# MVGen
+
+## What is this
+
+MVGen (Music Video Generator) dynamically generates music videos by putting together video chunks in a predefined, systematic manner. The resulting music video consistes of a many different scenes which are being alternated at a specific tempo, where the timing of the change is synced with the audio beat.
+
+## Installation
+
+- Clone the repo (`git clone`).
+- Deploy an HTTP server (any server will do, e.g. `python -m SimpleHTTPServer`).
+- Go to the `index.html` location in your browser (preferrably Chrome).
+
+## Usage
+
+The interface of MVGen consists of the following fields,
+
+- **Audio**: Audio file for the music video
+- **Videos**: Video chunks for the music video. This is the most tricky part. Each chunk represents a scene that will need to be alternated. The files need to be fragmented MP4 encoded with H.264/AAC. If you have a full video that you want to split up, you can use ffmpeg and command `ffmpeg -i <input> -c:v libx264 -c:a aac -f segment -segment_time <time> -segment_format_options movflags=empty_moov+default_base_moof+frag_keyframe -reset_timestamps 1 <output_%d.mp4>`, where `<input>` is the path to the full video, `<time>` is the segment duration and `<output_%d.mp4>` is the output files (`%d` in the name is a counter and ensure files have different names). Because of the way ffmpeg works, some resulting files may be shorter than the specified duration, so you may want to remove them.
+- **Video category**: You may store the video chunks in a directory for easier access, grouped by categories. The path is `videos/<category>/<duration>/**.mp4`, where `<category>` is the category name and `<duration`> is the duration of the shortest video in that folder. If you're running the server locally, it's no different from just uploading the files using the file input above, however it is much slower, because the speed is limited by how fast your server can serve them.
+- **Audio bpm**: bpm will be detected when a new audio is given but you can also change it afterwards.
+- **Multiplier**: Number of scenes per beat, e.g. if bpm is 128 and number of scenes is 1, the scene will be changing 128 times per minute.
+- **Offset**: Audio offset, that you can modify in case the video is out of sync with the audio. You can change this field during playback and it should be reflected immediately. You can also use arrows and keyboard to modify this.
+
+## Limitations
+
+- The capturing functionality is not entirely robust and only seems to work on Chrome.
+
+- The size of the video buffer is limited, therefore only SD videos can be used for the most part.
+
+## Thanks to
+
+- MediaSource and MediaRecorder API,
+- Web Audio API,
+- ffmpeg,
+- HTML5.
---
+++ main.js
@@ -0,0 +1,357 @@
+var detect = require('bpm-detective');
+var $ = require("jquery");
+var FileSaver = require('file-saver');
+
+var video = document.querySelector('video');
+var audio = document.querySelector('audio');
+var audioFile = document.getElementById("upload");
+var mimeCodec = 'video/mp4; codecs="avc1.42E01E, mp4a.40.2"';
+var bpm;
+var audioDuration;
+var loadFn;
+var duration;
+var delta;
+var N;
+var newAudio = false;
+var offset = 0;
+var videoSources;
+var context = new (window.AudioContext || window.webKitAudioContext)(); // Audio context
+
+if (!('MediaSource' in window && MediaSource.isTypeSupported(mimeCodec))) {
+  writeLine('Compatibility failure.');
+  console.error('Unsupported MIME type or codec: ', mimeCodec);
+}
+
+
+// Event listeners
+
+$(video).on('seeked', function() { audio.currentTime = this.currentTime + offset; });
+
+$(video).on('play', function() { audio.play(); });
+
+$(video).on('pause', function() { audio.pause(); });
+
+$('#offset').on('keyup keydown mouseup mousedown', function() {
+  if (this.value != offset) {
+    offset = parseFloat(this.value);
+    audio.currentTime = video.currentTime + offset;
+    writeLine('Audio offset: ' + roundDec(offset, 3));
+  };
+});
+
+$('.shell-body').on('contentChange', function() { this.scrollTop = this.scrollHeight; });
+
+$('#capture-button').click(captureVideo);
+
+$('#upload').on('change', function() { newAudio = true; });
+
+listDir('/videos/', function(dirs) { 
+  $.each(dirs, function(index, value) {
+    $('#videos').append($('<option>', {
+      value: value.slice(0, -1),
+      text: value.slice(0, -1)
+    }));
+  });
+});
+
+$('#videos option[value="0"]').remove();
+
+$('#video-upload').change(function (){
+  if (this.files.length > 0) { 
+    $('#video-category').addClass('disabled');
+  } else {
+    $('#video-category').removeClass('disabled');
+  }
+})
+
+$('.fa-trash-o').click(function() {
+  $('#video-upload').val([]); 
+  $('#video-category').removeClass('disabled');
+})
+
+addEventListener('error', function(event) { writeLine('ERROR: ' + event.message) })
+
+
+// Main functions
+
+$('#submit').on('click', function() {
+  if (newAudio) { 
+    // Need to run this part when audio is new even if user changes bpm manually
+    // to get audio duration
+    writeLine('Processing audio.')
+    var file = $('#upload')[0].files[0];
+    audio.src = URL.createObjectURL(file);
+    
+    // Reset frontend
+    $('#video-box').hide();
+    // $('.masked-element').show();
+    $('#progress-box').show();
+    offset = 0;
+    $('#offset').val(offset);
+    $('#progress-rect').height("100%");
+  
+    function get_bpm(callback) {
+      var reader = new FileReader();
+      reader.onload = function(ev) {
+        context.decodeAudioData(ev.target.result, function(buffer) {          
+          bpm = detect(buffer);
+          audioDuration = buffer.duration;
+          console.log('Audio duration: ' + audioDuration);
+          console.log('BPM: ' + bpm);
+          callback(bpm, audioDuration);
+        });
+      };
+      reader.readAsArrayBuffer(file);
+    }
+  
+    get_bpm(function(bpm, audioDuration){
+      $('#select-bpm').val(Math.round(bpm));      
+      makeVid();
+      newAudio = false;  
+    });      
+  } else {
+    makeVid();
+  }
+});
+
+function makeVid() {
+
+  // Handle frontend
+  $('#video-box').hide();
+  // $('.masked-element').show();  
+  $('#progress-box').show();
+  audio.pause();  
+  bpm = parseFloat($('#select-bpm').val()); // Do use rounded bpm  
+
+  writeLine('Audio BPM: ' + roundDec(bpm, 3));
+  
+  duration = 0;
+  var mult = parseFloat($('#select-mult').val())
+  delta = mult * 60 / bpm;
+  console.log('Delta: ' + delta)
+
+  writeLine('Loading video files...')
+
+  N = Math.ceil((audioDuration / delta));  
+
+  if ($('#video-upload')[0].files.length == 0) {
+  // if (1) {
+
+    var category = $('#videos').find(":selected").val();
+
+    listDir('videos/' + category + '/', function(deltas) {
+      deltaDir = closest(delta, deltas.map(x => parseFloat(x.slice(0, -1))));
+      listDir('videos/' + category + '/' + deltaDir, function(v) {
+        videoSources = v.map(x => 'videos/' + category + '/' + deltaDir + '/' + x)
+        videoSources = getRandom(videoSources, N);
+
+        loadFn = downloadData;
+
+        mediaSource = new MediaSource;
+        video.src = URL.createObjectURL(mediaSource);
+        mediaSource.addEventListener('sourceopen', sourceOpen);
+      })
+    })
+
+
+    // vidN = 9;
+    // videoSources = Array.apply(null, {length: vidN + 1}).map(Number.call, Number)
+    // videoSources = getRandom(videoSources, N);
+    // videoSources = videoSources.map(x => 'video/128_2/g_' + x + '.mp4')
+      
+    // loadFn = downloadData;
+
+  } else {
+
+    videoSources = Array.from($('#video-upload')[0].files);
+    videoSources = getRandom(videoSources, N);
+    
+    loadFn = loadData;    
+
+    mediaSource = new MediaSource;
+    video.src = URL.createObjectURL(mediaSource);
+    mediaSource.addEventListener('sourceopen', sourceOpen);
+  
+  } 
+}
+
+
+function sourceOpen(e) {
+
+  console.log('Adding SourceBuffer from MediaSource')
+
+  sourceBuffer = mediaSource.addSourceBuffer(mimeCodec);
+  sourceBuffer.mode = 'sequence';
+
+  console.log('SourceBuffer mode set to ' + sourceBuffer.mode);
+
+  sourceBuffer.addEventListener('updateend', function(e) {
+
+    if (videoSources.length == 0) {
+      console.log('Calling EOS');
+    
+      mediaSource.endOfStream();
+      video.currentTime = 0;     
+      $('#video-box').show();
+      $('.masked-element').hide();
+      $('#progress-box').hide();
+      play(video);
+
+      writeLine('Complete.');
+    
+      return;       
+    }
+
+    loadFn(videoSources.pop(), function(arrayBuffer) {
+      pc = 100 * (1 - (videoSources.length / N));
+      updateLine('Loading video files... ' + roundDec(pc, 3) + '%')
+      
+      console.log('Updating buffer at ' + duration);
+      sourceBuffer.timestampOffset = duration;
+      duration = duration + delta;
+      sourceBuffer.appendBuffer(arrayBuffer);
+    });    
+  });
+
+  loadFn(videoSources.pop(), function(arrayBuffer) {
+    console.log('Updating buffer at ' + duration);
+    duration = duration + delta;
+    sourceBuffer.appendBuffer(arrayBuffer);
+  });
+
+}
+
+function downloadData(url, cb) {
+  console.log("Downloading " + url);  
+  var xhr = new XMLHttpRequest;
+  xhr.open('get', url);
+  xhr.responseType = 'arraybuffer';
+  xhr.onload = function () {
+    cb(new Uint8Array(xhr.response));
+  };
+  xhr.send();
+}
+
+function loadData(file, callback) {
+  var reader = new FileReader();
+  reader.onload = function(ev) {
+    callback(ev.target.result);
+  };
+  reader.readAsArrayBuffer(file);
+}
+
+function getRandom(arr, n) {
+  // https://stackoverflow.com/questions/19269545/how-to-get-n-no-elements-randomly-from-an-array?lq=1
+  var result = new Array(n),
+      len = arr.length,
+      taken = new Array(len);
+  while (n--) {
+    var x = Math.floor(Math.random() * len);
+    result[n] = arr[x];
+  }
+  return result;
+}
+
+function replicateArray(array, n) {
+  // https://stackoverflow.com/questions/30228902/duplicate-an-array-an-arbitrary-number-of-times-javascript
+  var arrays = Array.apply(null, new Array(n));
+  arrays = arrays.map(function() { return array });
+  return [].concat.apply([], arrays);
+}
+
+function writeLine(text) { 
+  var $li = $("<li>", {"class": "typewriter"}).html(text);
+  $('.shell-body').append($li);
+  
+  $('.shell-body').trigger('contentChange');
+
+}
+
+function updateLine(text) {
+  $('.shell-body').find('li').last().html(text);
+}
+
+function roundDec(num, i) {
+  return Math.round(num * 10**i) / 10**i;
+}
+
+var capturing = false;
+function captureVideo() {
+  if (!capturing) {
+    capturing = true;
+
+    if (typeof video.captureStream !== "undefined") { 
+      stream = video.captureStream();
+      audioStream = audio.captureStream();
+
+      stream.addTrack(audioStream.getAudioTracks()[0]);
+      stream.removeTrack(stream.getAudioTracks()[0]);
+           
+      mediaRecorder = new MediaRecorder(stream);
+      chunks = [];
+      mediaRecorder.ondataavailable = function (e) {
+          chunks.push(e.data);
+      };
+
+      video.currentTime = 0;
+      play(video);
+      mediaRecorder.start();
+      $('video').on('ended', function() {mediaRecorder.stop()})
+
+      $('#capture-button').text('Stop capturing').toggleClass('btn-primary btn-danger');
+      
+      mediaRecorder.onstop = function(e) {
+        console.log("MediaRecorder.stop() was called.");
+        var blob = new Blob(chunks, {'type': 'video/webm'});
+        var blobURL = URL.createObjectURL(blob);
+        writeLine('Download: <a href="' + blobURL + '">' + blobURL + '</a>');
+        $('#capture-button').text('Capture video').toggleClass('btn-danger btn-primary');
+        FileSaver.saveAs(blob, $('#upload')[0].files[0].name + '.webm');
+        pause(video);
+        $('video').off('ended');
+      }
+    } else {
+      $('#capture-button').text("Your browser doesn't this feature (try Chrome)")
+    }
+  } else {
+    capturing = false;
+    mediaRecorder.stop();
+  } 
+}
+
+
+function play(video) {
+  var isPlaying = video.currentTime > 0 && !video.paused && !video.ended && video.readyState > 2;
+
+  if (!isPlaying) {
+    video.play();
+  }
+}
+
+function pause(video) {
+  var playPromise = video.play();
+ 
+  if (playPromise !== undefined) {
+    playPromise.then(_ => {
+      video.pause();
+    })
+  }
+}
+
+function listDir(path, cb) {
+  var fileNames = new Array();
+  $.ajax({
+    url: path,
+    success: function(data){
+      $(data).find("a").each(function(){
+        var name = $(this).attr("href");
+        fileNames.push(name);
+      });
+      cb(fileNames);
+    }
+  }); 
+}
+
+function closest(x, array) {
+  return array.sort((a, b) => Math.abs(x - a) - Math.abs(x - b))[0]
+}
